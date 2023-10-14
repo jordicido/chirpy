cd4eb9c9b6b3e4e0765ca5e4ad7382d6eaa7d7f2
jordicido <jordi.cidoncha4@gmail.com>
2023-10-14 08:32:36 +0200

    logic between front and api decoupled

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/app", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.Handle("/app/assets/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", healthzHandler)
+	r := chi.NewRouter()
+	apiR := chi.NewRouter()
+	var apiCfg apiConfig
 
-	corsMux := middlewareCors(mux)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	r.Handle("/app", fsHandler)
+	r.Handle("/app/*", fsHandler)
+
+	apiR.Get("/healthz", healthzHandler)
+	apiR.Get("/metrics", apiCfg.metricsHandler)
+	apiR.HandleFunc("/reset", apiCfg.resetHandler)
+
+	r.Mount("/api", apiR)
+	corsMux := middlewareCors(r)
 
 	httpServer := &http.Server{
 		Addr:    ":8080",
@@ -25,6 +42,25 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits += 1
+		next.ServeHTTP(w, r)
+	})
 }
 
 func middlewareCors(next http.Handler) http.Handler {
